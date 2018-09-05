91b4e58695bab254430b895da7331564407cede8
evi1haxor <architdwivedi.off@gmail.com>
2018-09-05 11:54:27 +0530

    made complex algorithm for swapping values of two numbers

---
+++ swap.c
@@ -0,0 +1,35 @@
+/* a complex algorithm of swapping the values of two numbers */
+
+#include<stdio.h>
+int main(){
+	int a,b,s=0,l,m;
+	scanf("%d%d",&a,&b);
+	if(a>b){
+		l=a;
+		m=b;
+	}
+	else{
+		l=b;
+		m=a;
+	}
+	for(int i=0;i<=l;i++){
+		if(i==m){
+			m++;
+			s++;
+		}
+	}
+	l-=s;
+	l++;
+	m--;
+	if(a>b){
+		a=l;
+		b=m;
+	}
+	else{
+		a=m;
+		b=l;
+	}
+	printf("%d %d",a,b);
+
+	return 0;
+}
